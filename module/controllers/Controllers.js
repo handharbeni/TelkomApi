926da912a845e0a3aab169776a9414ee530d9249
handharbeni <mhandharbeni@gmail.com>
2018-11-02 23:26:00 +0700

    Update Content

--- module/controllers/Controllers.js
+++ module/controllers/Controllers.js
@@ -139,7 +139,7 @@ exports.saveBlog = function(req, res, next){
     var property = {
         title: req.body.title,
         description: req.body.description,
-        content: req.body.content,
+        content: '<html><meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><meta name="HandheldFriendly" content="true"><body>'+req.body.content+'</body></html>',
         date: Date.now(),
         dateModified: Date.now(),
         thumbnails: req.body.thumbnails,
